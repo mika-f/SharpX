b1c290a36f1a2a32a4bb10468aebff593ecc90c0
Natsune Mochizuki <me@natsuneko.cat>
2022-04-25 20:20:39 +0900

    feat'hlsl): add base classes

---
+++ src/SharpX.Hlsl/Syntax/BaseTypeDeclarationSyntax.cs
@@ -0,0 +1,20 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using SharpX.Core;
+using SharpX.Hlsl.Syntax.InternalSyntax;
+
+namespace SharpX.Hlsl.Syntax;
+
+public abstract class BaseTypeDeclarationSyntax : MemberDeclarationSyntax
+{
+    public abstract SyntaxToken Identifier { get; }
+
+    public abstract SyntaxToken OpenBraceToken { get; }
+
+    public abstract SyntaxToken CloseBraceToken { get; }
+
+    internal BaseTypeDeclarationSyntax(HlslSyntaxNodeInternal node, SyntaxNode? parent, int position) : base(node, parent, position) { }
+}
---
+++ src/SharpX.Hlsl/Syntax/TypeDeclarationSyntax.cs
@@ -0,0 +1,18 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using SharpX.Core;
+using SharpX.Hlsl.Syntax.InternalSyntax;
+
+namespace SharpX.Hlsl.Syntax;
+
+public abstract class TypeDeclarationSyntax : BaseTypeDeclarationSyntax
+{
+    public abstract SyntaxToken Keyword { get; }
+
+    public abstract SyntaxList<MemberDeclarationSyntax> Members { get; }
+
+    internal TypeDeclarationSyntax(HlslSyntaxNodeInternal node, SyntaxNode? parent, int position) : base(node, parent, position) { }
+}
