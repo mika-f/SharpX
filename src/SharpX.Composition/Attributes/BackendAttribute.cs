2a3f495ee821c4342b4e41219d5a320364f667cb
Natsune Mochizuki <me@natsuneko.cat>
2022-04-29 13:27:27 +0900

    feat(composition): add plugin interface

---
+++ src/SharpX.Composition/Attributes/BackendAttribute.cs
@@ -0,0 +1,9 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+namespace SharpX.Composition.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class BackendAttribute : Attribute { }
---
+++ src/SharpX.Composition/Interfaces/IBackendVisitorArgs.cs
@@ -0,0 +1,13 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using Microsoft.CodeAnalysis;
+
+namespace SharpX.Composition.Interfaces;
+
+public interface IBackendVisitorArgs
+{
+    SemanticModel SemanticModel { get; }
+}
