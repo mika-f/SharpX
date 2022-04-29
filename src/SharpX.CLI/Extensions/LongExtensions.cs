9bdf3a4fca5a0cd700ecfc1724f305e0b19d833d
Natsune Mochizuki <me@natsuneko.cat>
2022-04-29 13:35:46 +0900

    fix(cli): fix style

--- src/SharpX.CLI/Extensions/LongExtensions.cs
+++ src/SharpX.CLI/Extensions/LongExtensions.cs
@@ -1,14 +1,18 @@
-﻿namespace SharpX.CLI.Extensions
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+namespace SharpX.CLI.Extensions;
+
+internal static class LongExtensions
 {
-    internal static class LongExtensions
+    public static string ToReadableString(this long elapsed)
     {
-        public static string ToReadableString(this long elapsed)
-        {
-            if (elapsed < 1000)
-                return $"{elapsed} ms";
-            if (elapsed < 1000 * 60)
-                return $"{elapsed / 1000} s";
-            return $"{elapsed / 1000 * 60} m";
-        }
+        if (elapsed < 1000)
+            return $"{elapsed} ms";
+        if (elapsed < 1000 * 60)
+            return $"{elapsed / 1000} s";
+        return $"{elapsed / 1000 * 60} m";
     }
 }
