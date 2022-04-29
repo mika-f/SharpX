64093327c802ae8908672026d0901ec6c0fbfc7c
Natsune Mochizuki <me@natsuneko.cat>
2022-04-29 13:41:32 +0900

    feat(csharp): add base plugin

---
+++ src/SharpX.Hlsl.CSharp/PluginEntryPoint.cs
@@ -0,0 +1,21 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using System.Diagnostics;
+
+using SharpX.Composition.Attributes;
+using SharpX.Composition.Interfaces;
+
+namespace SharpX.Hlsl.CSharp;
+
+[Backend]
+public class PluginEntryPoint : IBackend
+{
+    public void EntryPoint(IBackendRegistry registry)
+    {
+        Debug.WriteLine("");
+        registry.RegisterBackendVisitor("HLSL", typeof(NodeVisitor), 0);
+    }
+}
