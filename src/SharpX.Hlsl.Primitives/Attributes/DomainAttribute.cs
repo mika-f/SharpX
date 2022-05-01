f44f098524e495541ae930c1c4cb3b5836c95b03
Natsune Mochizuki <me@natsuneko.cat>
2022-05-01 12:52:30 +0900

    fix(hlsl): fix license header

--- src/SharpX.Hlsl.Primitives/Attributes/DomainAttribute.cs
+++ src/SharpX.Hlsl.Primitives/Attributes/DomainAttribute.cs
@@ -1,4 +1,9 @@
-﻿using SharpX.Hlsl.Primitives.Enums;
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using SharpX.Hlsl.Primitives.Enums;
 
 namespace SharpX.Hlsl.Primitives.Attributes;
 
