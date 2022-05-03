db8bdeb90ff6bc5ed66a513c6858da25f843c3ee
Natsune Mochizuki <me@natsuneko.cat>
2022-05-03 14:44:40 +0900

    feat(hlsl): add compiler attributes

---
+++ src/SharpX.Hlsl.Primitives/Attributes/Compiler/ComponentAttribute.cs
@@ -0,0 +1,12 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+namespace SharpX.Hlsl.Primitives.Attributes.Compiler;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface)]
+public class ComponentAttribute : Attribute
+{
+    public ComponentAttribute(string signature) { }
+}
---
+++ src/SharpX.Hlsl.Primitives/Attributes/Compiler/NameAttribute.cs
@@ -0,0 +1,12 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+namespace SharpX.Hlsl.Primitives.Attributes.Compiler;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field)]
+public class NameAttribute : Attribute
+{
+    public NameAttribute(string name) { }
+}
