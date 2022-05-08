bcd4c002ae3777c2638e31203d9675f9d234af01
Natsune Mochizuki <me@natsuneko.cat>
2022-05-08 16:57:12 +0900

    fix(shaderlab): fix license header

--- src/SharpX.ShaderLab/Syntax/InternalSyntax/CustomEditorDeclarationSyntaxInternal.cs
+++ src/SharpX.ShaderLab/Syntax/InternalSyntax/CustomEditorDeclarationSyntaxInternal.cs
@@ -1,62 +1,66 @@
-﻿using Microsoft.CodeAnalysis;
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using Microsoft.CodeAnalysis;
 
 using SharpX.Core;
 
 using SyntaxNode = SharpX.Core.SyntaxNode;
 
-namespace SharpX.ShaderLab.Syntax.InternalSyntax
+namespace SharpX.ShaderLab.Syntax.InternalSyntax;
+
+internal class CustomEditorDeclarationSyntaxInternal : ShaderLabSyntaxNodeInternal
 {
-    internal class CustomEditorDeclarationSyntaxInternal : ShaderLabSyntaxNodeInternal
+    public SyntaxTokenInternal CustomEditorKeyword { get; }
+
+    public SyntaxTokenInternal FullyQualifiedInspectorName { get; }
+
+    public CustomEditorDeclarationSyntaxInternal(SyntaxKind kind, SyntaxTokenInternal customEditorKeyword, SyntaxTokenInternal fullyQualifiedInspectorName) : base(kind)
     {
-        public SyntaxTokenInternal CustomEditorKeyword { get; }
+        SlotCount = 2;
 
-        public SyntaxTokenInternal FullyQualifiedInspectorName { get; }
+        AdjustWidth(customEditorKeyword);
+        CustomEditorKeyword = customEditorKeyword;
 
-        public CustomEditorDeclarationSyntaxInternal(SyntaxKind kind, SyntaxTokenInternal customEditorKeyword, SyntaxTokenInternal fullyQualifiedInspectorName) : base(kind)
+        AdjustWidth(fullyQualifiedInspectorName);
+        FullyQualifiedInspectorName = fullyQualifiedInspectorName;
+    }
+
+    public CustomEditorDeclarationSyntaxInternal(SyntaxKind kind, SyntaxTokenInternal customEditorKeyword, SyntaxTokenInternal fullyQualifiedInspectorName, DiagnosticInfo[]? diagnostics, SyntaxAnnotation[]? annotations) : base(kind, diagnostics, annotations)
+    {
+        SlotCount = 2;
+
+        AdjustWidth(customEditorKeyword);
+        CustomEditorKeyword = customEditorKeyword;
+
+        AdjustWidth(fullyQualifiedInspectorName);
+        FullyQualifiedInspectorName = fullyQualifiedInspectorName;
+    }
+
+    public override GreenNode SetAnnotations(SyntaxAnnotation[]? annotations)
+    {
+        return new CustomEditorDeclarationSyntaxInternal(Kind, CustomEditorKeyword, FullyQualifiedInspectorName, GetDiagnostics(), annotations);
+    }
+
+    public override GreenNode SetDiagnostics(DiagnosticInfo[]? diagnostics)
+    {
+        return new CustomEditorDeclarationSyntaxInternal(Kind, CustomEditorKeyword, FullyQualifiedInspectorName, diagnostics, GetAnnotations());
+    }
+
+    public override GreenNode? GetSlot(int index)
+    {
+        return index switch
         {
-            SlotCount = 2;
+            0 => CustomEditorKeyword,
+            1 => FullyQualifiedInspectorName,
+            _ => null
+        };
+    }
 
-            AdjustWidth(customEditorKeyword);
-            CustomEditorKeyword = customEditorKeyword;
-
-            AdjustWidth(fullyQualifiedInspectorName);
-            FullyQualifiedInspectorName = fullyQualifiedInspectorName;
-        }
-
-        public CustomEditorDeclarationSyntaxInternal(SyntaxKind kind, SyntaxTokenInternal customEditorKeyword, SyntaxTokenInternal fullyQualifiedInspectorName, DiagnosticInfo[]? diagnostics, SyntaxAnnotation[]? annotations) : base(kind, diagnostics, annotations)
-        {
-            SlotCount = 2;
-
-            AdjustWidth(customEditorKeyword);
-            CustomEditorKeyword = customEditorKeyword;
-
-            AdjustWidth(fullyQualifiedInspectorName);
-            FullyQualifiedInspectorName = fullyQualifiedInspectorName;
-        }
-
-        public override GreenNode SetAnnotations(SyntaxAnnotation[]? annotations)
-        {
-            return new CustomEditorDeclarationSyntaxInternal(Kind, CustomEditorKeyword, FullyQualifiedInspectorName, GetDiagnostics(), annotations);
-        }
-
-        public override GreenNode SetDiagnostics(DiagnosticInfo[]? diagnostics)
-        {
-            return new CustomEditorDeclarationSyntaxInternal(Kind, CustomEditorKeyword, FullyQualifiedInspectorName, diagnostics, GetAnnotations());
-        }
-
-        public override GreenNode? GetSlot(int index)
-        {
-            return index switch
-            {
-                0 => CustomEditorKeyword,
-                1 => FullyQualifiedInspectorName,
-                _ => null
-            };
-        }
-
-        public override SyntaxNode CreateRed(SyntaxNode? parent, int position)
-        {
-            return new CustomEditorDeclarationSyntax(this, parent, position);
-        }
+    public override SyntaxNode CreateRed(SyntaxNode? parent, int position)
+    {
+        return new CustomEditorDeclarationSyntax(this, parent, position);
     }
 }
