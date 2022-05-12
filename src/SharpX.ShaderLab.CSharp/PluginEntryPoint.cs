11319cb92535c3215a6d10500429b910e9e6b227
Natsune Mochizuki <me@natsuneko.cat>
2022-05-12 22:35:25 +0900

    fix(shaderlab): fix plugin entrypoint

--- src/SharpX.ShaderLab.CSharp/PluginEntryPoint.cs
+++ src/SharpX.ShaderLab.CSharp/PluginEntryPoint.cs
@@ -3,28 +3,18 @@
 //  Licensed under the MIT License. See LICENSE in the project root for license information.
 // ------------------------------------------------------------------------------------------
 
+using SharpX.Composition.Attributes;
 using SharpX.Composition.Interfaces;
-using SharpX.Hlsl;
-
-using HlslNodeVisitor = SharpX.Hlsl.CSharp.NodeVisitor;
 
 namespace SharpX.ShaderLab.CSharp;
 
+[Backend]
 public class PluginEntryPoint : IBackend
 {
     public void EntryPoint(IBackendRegistry registry)
     {
         registry.RegisterBackendVisitor("ShaderLab", typeof(ShaderLabNodeVisitor), typeof(ShaderLabSyntaxNode), 0);
-        registry.RegisterBackendVisitor("ShaderLab", typeof(HlslNodeVisitor), typeof(HlslSyntaxNode), 1);
-
         registry.RegisterReferences("ShaderLab", "./SharpX.ShaderLab.Primitives.dll");
-        registry.RegisterReferences("ShaderLab", "./SharpX.Hlsl.Primitives.dll");
-
-        registry.RegisterExtensions("ShaderLab", node =>
-        {
-            if (node is HlslSyntaxNode)
-                return "cginc";
-            return "shader";
-        });
+        registry.RegisterExtensions("ShaderLab", _ => "shader");
     }
 }
