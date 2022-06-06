29876ca39abb689071dcf2dee61c8b9708d2c247
Natsune Mochizuki <me@natsuneko.cat>
2022-06-06 13:15:09 +0900

    feat(hlsl): support enum

---
+++ src/HLSL/SharpX.Hlsl.CSharp.Enum/HlslNodeVisitor.cs
@@ -0,0 +1,40 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using Microsoft.CodeAnalysis;
+
+using SharpX.Composition.CSharp;
+using SharpX.Composition.Interfaces;
+using SharpX.Hlsl.Syntax;
+
+using MemberAccessExpressionSyntax = Microsoft.CodeAnalysis.CSharp.Syntax.MemberAccessExpressionSyntax;
+
+namespace SharpX.Hlsl.CSharp.Enum;
+
+internal class HlslNodeVisitor : CompositeCSharpSyntaxVisitor<HlslSyntaxNode>
+{
+    private static readonly Core.SyntaxList<AttributeListSyntax> EmptyAttributes = SyntaxFactory.List<AttributeListSyntax>();
+    private readonly IBackendVisitorArgs<HlslSyntaxNode> _args;
+
+    public HlslNodeVisitor(IBackendVisitorArgs<HlslSyntaxNode> args) : base(args)
+    {
+        _args = args;
+    }
+
+    public override HlslSyntaxNode? VisitMemberAccessExpression(MemberAccessExpressionSyntax oldNode, HlslSyntaxNode? newNode)
+    {
+        var m = _args.SemanticModel.GetSymbolInfo(oldNode);
+        if (m.Symbol is not IFieldSymbol symbol)
+            return newNode;
+
+        if (symbol.IsConst)
+        {
+            var val = _args.SemanticModel.GetConstantValue(oldNode).Value!.ToString()!;
+            return SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression, SyntaxFactory.Literal(val));
+        }
+
+        return newNode;
+    }
+}
