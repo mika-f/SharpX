4259d08e313d0d7b2f58b05aa9eb3fb5ee729edc
Natsune Mochizuki <me@natsuneko.cat>
2022-07-04 18:23:02 +0900

    fix(shaderlab): fix endpoint

--- src/ShaderLab/SharpX.ShaderLab/PluginEntryPoint.cs
+++ src/ShaderLab/SharpX.ShaderLab/PluginEntryPoint.cs
@@ -11,7 +11,7 @@ namespace SharpX.ShaderLab;
 [Backend]
 public class PluginEntryPoint : IBackend
 {
-    public string ContextId => "HLSL";
+    public void EntryPoint(IBackendRegistry registry)
     {
         // nop
     }
